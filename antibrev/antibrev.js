2da90ebc5472e329a31886a58772ec13ca80c29f
HsuYiRen <hyr1502@gmail.com>
2020-08-11 14:42:54 +0800

    Add antibrev.js

---
+++ antibrev/antibrev.js
@@ -0,0 +1,27 @@
+
+let requestURL = 'https://hsuyr.github.io/medtools/antibrev/abbrevs.json';
+
+let request = new XMLHttpRequest();
+
+request.open('GET', requestURL);
+
+request.responseType = 'json';
+request.send();
+
+request.onload = ()=>{
+    const abbrevs = request.response;
+    abbrevs = Object.keys(abbrevs).map(key => [new RegExp('(^|[^a-zA-Z])'+key.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')+'([^a-zA-Z]|$)', 'g'), `${abbrevs[key]} (${key})`]);
+    abbrevs = new Map(abbrevs);
+
+    let note = document.querySelector('#cke_1_contents iframe').contentDocument;
+    let paragraphs = note.querySelectorAll('p[style="font-family: Courier New;"]');
+
+    paragraphs.forEach(e => {
+    for (let [re, fullName] of abbrevs) {
+        e.textContent = e.textContent.replace(re, `$1${fullName}$2`);
+
+    }
+    });
+};
+
+
